849841e6596d9f2c89d32d8a572f2cddee3c1ee1
Jonathan Zinger <jzinger1@babson.edu>
2017-05-19 23:26:18 -0400

    [modified] webpack config for heroku

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('/', function(req,res) {
 	res.sendFile(path.join(__dirname, 'build/index.html'));
 });
 
-app.listen(PROCESS.ENV.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function() {
 	console.log('running at port:3000');
 });
